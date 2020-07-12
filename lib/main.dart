ec6740e25a52e42a6f5b3fe27376d3508bf88a51
agusaditya <adityaagus537@gmail.com>
2020-07-12 14:21:37 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:financial_note/inherited_widgets/note_inherited_widget.dart';
+import 'package:financial_note/views/note_list.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return NoteInheritedWidget(
+      MaterialApp(
+        title: 'Notes',
+        home: NoteList(),
+      ),
+    );
+  }
+}
